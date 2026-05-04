a94dbbdd042b4fedc851e0f3f59b7b138ea4d468
lyanyu975-boop <lyanyu975@gmail.com>
2026-05-04 10:13:57 +0800

    更正

--- sketch.js
+++ sketch.js
@@ -4,20 +4,28 @@ let faces = [];
 let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
 
 function preload() {
-  // 載入 ml5 faceMesh 模型
-  faceMesh = ml5.faceMesh(options);
+  // 檢查 ml5 是否存在，避免 ReferenceError
+  if (typeof ml5 !== 'undefined') {
+    faceMesh = ml5.faceMesh(options);
+  } else {
+    console.error("錯誤：ml5 未定義。請確保 HTML 檔案中已引入 ml5.js 程式庫。");
+  }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
   // 建立攝影機擷取
-  video = createCapture(VIDEO);
+  video = createCapture(VIDEO, (stream) => {
+    console.log("攝影機已啟動");
+  });
   video.size(640, 480);
-  video.hide(); // 隱藏預設的 HTML 影片元件
+  video.hide();
 
   // 開始偵測臉部
-  faceMesh.detectStart(video, gotFaces);
+  if (faceMesh) {
+    faceMesh.detectStart(video, gotFaces);
+  }
 }
 
 function draw() {
@@ -39,21 +47,22 @@ function draw() {
   // 3. 處理鏡像並繪製影像與臉譜
   push();
   // 移動到顯示區域的右側並反轉 X 軸，以修正左右顛倒問題
-  translate(x + displayW, y);
+  translate(x + displayW, y); 
   scale(-1, 1);
 
   // 繪製攝影機影像
   image(video, 0, 0, displayW, displayH);
 
-  // 繪製臉譜特徵點
-  for (let i = 0; i < faces.length; i++) {
-    let face = faces[i];
-    for (let j = 0; j < face.keypoints.length; j++) {
-      let keypoint = face.keypoints[j];
-      fill(0, 255, 0);
-      let kx = map(keypoint.x, 0, video.width, 0, displayW);
-      let ky = map(keypoint.y, 0, video.height, 0, displayH);
-      circle(kx, ky, 3);
+  // 繪製臉譜特徵點 (確保影像比例對齊)
+  if (faces.length > 0) {
+    let face = faces[0];
+    fill(0, 255, 0);
+    noStroke();
+    for (let i = 0; i < face.keypoints.length; i++) {
+      let kp = face.keypoints[i];
+      let kx = map(kp.x, 0, video.width, 0, displayW);
+      let ky = map(kp.y, 0, video.height, 0, displayH);
+      circle(kx, ky, 2);
     }
   }
   pop();
